96c4191d3e03883604852262c646b905dde6eaa8
jl8771 <jlxpi123@gmail.com>
2021-06-28 12:58:00 -0400

    Add voice support

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const fs = require('fs');
+const ytdl = require('ytdl-core');
 const { TwitterApi } = require("twitter-api-v2");
 const { MessageEmbed, Client, MessageAttachment } = require("discord.js")
 const fetch = require('node-fetch');
@@ -11,21 +12,46 @@ require('dotenv').config();
 const twitterClient = new TwitterApi(process.env.BEARER_TOKEN);
 const roClient = twitterClient.readOnly;
 
-var tweetText;
-var tweetId;
-var tweetUser;
 var NOTIFY_CHANNEL;
+var TEST_CHANNEL;
+var TEST_VC;
 var startDate = new Date();
 
 client.on('ready', () => {
   client.user.setActivity("Now Online"); //Sets game playing
   console.log(`Logged in as ${client.user.tag}! at ` + startDate.getHours() + ":" + (startDate.getMinutes()<10?'0':'') + startDate.getMinutes());
   NOTIFY_CHANNEL = client.channels.cache.get('858555302483460176');
+  TEST_CHANNEL = client.channels.cache.get('858546770199969792');
+  TEST_VC = client.channels.cache.get('858065557248278541');
 });
+
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+async function testClock() {
+  let fixDate = new Date();
+  var bongTimes;
+  await sleep((60 - fixDate.getSeconds())* 1000);
+  while (true) {
+    let currDate = new Date();
+    if (currDate.getMinutes() == 0) {
+      currDate.getHours()>12?bongTimes=currDate.getHours()-12:bongTimes=currDate.getHours();
+      const connection = await TEST_VC.join();
+      const dispatcher = connection.play(fs.createReadStream('./audio/output1.ogg'), {
+        type: 'ogg/opus',
+      });
+      dispatcher.on('finish', () => {
+        connection.disconnect();
+      });
+    }
+    await sleep(60 * 1000);
+  }
+}
 
 var lastTweetId = '0';
 setInterval(function() {
-  let result = getTweetInfo('Rainbow6Game').then(value => {
+  getTweetInfo('LCSOfficial').then(value => {
     if (value[4] === lastTweetId) {
       console.log("Already tweeted " + lastTweetId);
     }else {
@@ -52,7 +78,7 @@ setInterval(function() {
     }
     return value;
   });
-}, 5 * 1000);
+}, 120 * 1000);
 
 async function getTweetInfo(username) {
   var displayImageUrl;
@@ -63,22 +89,28 @@ async function getTweetInfo(username) {
   const tweets = await twitterClient.v2.get('tweets?ids='+tweetStream.data[0].id+'&expansions=attachments.media_keys' + 
                                                                                   '&tweet.fields=created_at,public_metrics' + 
                                                                                   '&media.fields=height,width,url,preview_image_url').catch((err) => console.log(" "));
-  switch(tweets.includes.media[0].type) {
-    case 'photo':
-      displayImageUrl = tweets.includes.media[0].url;
-      displayImageType = "Photo"
-      break;
-    case 'animated_gif':
-      displayImageUrl = tweets.includes.media[0].preview_image_url;
-      displayImageType = "GIF"
-      break;
-    case 'video':
-      displayImageUrl = tweets.includes.media[0].preview_image_url;
-      displayImageType = "Video"
-      break;
-    default:
-      displayImageUrl = '';
-      displayImageType = "None"
+  
+  if ('attachments' in tweets.data[0] || 'includes' in tweets) {
+    switch(tweets.includes.media[0].type) {
+      case 'photo':
+        displayImageUrl = tweets.includes.media[0].url;
+        displayImageType = "Photo"
+        break;
+      case 'animated_gif':
+        displayImageUrl = tweets.includes.media[0].preview_image_url;
+        displayImageType = "GIF"
+        break;
+      case 'video':
+        displayImageUrl = tweets.includes.media[0].preview_image_url;
+        displayImageType = "Video"
+        break;
+      default:
+        displayImageUrl = '';
+        displayImageType = "None"
+    }
+  }else {
+    displayImageUrl = '';
+    displayImageType = "None"
   }
   var date = new Date(tweets.data[0].created_at);
   var dateStr = date.getFullYear() + "-" + date.getMonth() + "-" + date.getDate() + " " + 
@@ -105,85 +137,180 @@ async function getTweetInfo(username) {
 	}
 }*/
 
-const location = "ottawa";
-const tag = "ottawa";
-var weathers;
-var news;
+var location = "ottawa";
+//const tag = "ottawa";
+var weather;
+//var news;
 
-const urls = [
-  'http://api.openweathermap.org/data/2.5/weather?q='+location+'&appid=d1118046076544b562399e8deaf8653a',
-  'https://newsapi.org/v2/everything?q='+tag+'&from='+startDate.getFullYear()+'-'+startDate.getMonth()+'-'+startDate.getDate()+'&sortBy=popularity&pageSize=6&apiKey=7a4826f801d84e739cf7292a7e78f597'
-];
-
-Promise.all(urls.map(url =>
-  fetch(url)
-    .then(checkStatus)
-    .then(parseJSON)
-    .catch(error => console.log('There was a problem!', error))
-  ))
-  .then(data => {
-    weathers = data[0];
-})
-
-function checkStatus(response) {
-  if (response.ok) {
-    return Promise.resolve(response);
-  } else {
-    return Promise.reject(new Error(response.statusText));
-  }
-}
-
-function parseJSON(response) {
-  return response.json();
-}
-
-async function getTwitterInfo(){
-  getTweetUser().then(valueRes => {
-    tweetUser = valueRes
-  }, reason => {
-    console.error(reason);
-  });
-  getTweetId().then(valueRes => {
-    tweetId = valueRes
-  }, reason => {
-    console.error(reason);
-  });
-  getTweetText().then(valueRes => {
-    tweetText = valueRes
-  }, reason => {
-    console.error(reason);
-  });  
-
-}
-
-const file = new MessageAttachment('../src/sun.png');
+//'https://newsapi.org/v2/everything?q='+tag+'&from='+startDate.getFullYear()+'-'+startDate.getMonth()+'-'+startDate.getDate()+'&sortBy=popularity&pageSize=6&apiKey=7a4826f801d84e739cf7292a7e78f597'
 
 client.on('message', async message => {
   const input = message.content.slice(PREFIX.length).trim().split(' ');
-		const command = input.shift();
-		const commandArgs = input.join(' ');
+		const command = input.shift().toLowerCase();
+		const commandArgs = input.join('-');
 
 		if (command === 'a') {
-      message.channel.send("a " + client.channels.cache.findKey());
-		} else if (command === 'b') {
-			// [epsilon]
+      if (!message.guild) return;
+        // Only try to join the sender's voice channel if they are in one themselves
+        if (message.member.voice.channel) {
+          const connection = await message.member.voice.channel.join();
+          connection.play(fs.createReadStream('./audio/output1.ogg'), {
+            type: 'ogg/opus',
+          });
+        } else {
+          message.reply('You need to join a voice channel first!');
+        }
+		} else if (command === 'bong') {
+      testClock();
+      message.reply("Bonging now enabled! Next bong in " + (60 - startDate.getMinutes()) + " minutes!");
+		} else if (command === 'uwu') {
+      if (message.member.voice.channel) {
+        const connection = await message.member.voice.channel.join();
+        const dispatcher = connection.play(ytdl('https://www.youtube.com/watch?v=qZHsR2-pPEo', { filter: 'audioonly' }));
+        dispatcher.on('finish', () => {
+          connection.disconnect();
+        });
+      } else {
+        message.reply('You need to join a voice channel first!');
+      }
+		} else if (command === 'gl') {
+      if (message.member.voice.channel) {
+        const connection = await message.member.voice.channel.join();
+        const dispatcher = connection.play(ytdl('https://www.youtube.com/watch?v=aB2yqeD0Nus', { filter: 'audioonly' }));
+        dispatcher.on('finish', () => {
+          connection.disconnect();
+        });
+      } else {
+        message.reply('You need to join a voice channel first!');
+      }
+		} else if (command === 'play') {
+      if (message.member.voice.channel) {
+        const connection = await message.member.voice.channel.join();
+        const dispatcher = connection.play(ytdl(commandArgs, { filter: 'audioonly' }));
+        dispatcher.on('finish', () => {
+          connection.disconnect();
+        });
+      } else {
+        message.reply('You need to join a voice channel first!');
+      }
+      message.reply("Now playing - " + commandArgs);
 		} else if (command === 'c') {
-      
-		} else if (command === 'd') {
-			
+      const connection = await TEST_VC.join();
+      const dispatcher = connection.play(fs.createReadStream('./audio/output1.ogg'), {
+        type: 'ogg/opus',
+      });
+      dispatcher.on('finish', () => {
+        connection.disconnect();
+      });
 		} else if (command === 'e') {
-			
+			 
+		} else if (command === 'weather') {
+      location = commandArgs;
+      var displayIcon;
+			fetch('http://api.openweathermap.org/data/2.5/weather?q='+location+'&appid=d1118046076544b562399e8deaf8653a')
+        .then(res => res.json())
+        .then(json => {
+          switch(json.weather[0].id) {
+            case 200: case 201: case 202: case 210: case 211: case 212: case 221: case 230: case 231: case 232:
+                displayIcon = '/thunderstorm.png';
+                iconAlt = 'Thunderstorm';
+                break;
+            case 300: case 301: case 302: case 310: case 311: case 312: case 313: case 314: case 321:
+                displayIcon = '/shower.png';
+                iconAlt = "Drizzle"
+                break;
+            case 500: case 501: 
+                displayIcon = '/rain.png';
+                iconAlt = 'Rain';
+                break;
+            case 502: case 503: case 504:
+                displayIcon = '/rain.png';
+                iconAlt = 'Heavy Rain';
+                break;
+            case 511:
+                displayIcon = '/snow.png';
+                iconAlt = 'Freezing Rain';
+                break;
+            case 520: case 521: case 522: case 531:
+                displayIcon = '/shower.png';
+                iconAlt = 'Shower Rain';
+                break;
+            case 600: case 601: case 612: case 613: case 615: case 616: case 620: case 621:
+                displayIcon = '/snow.png';
+                iconAlt = 'Snow';
+                break;
+            case 602: case 611: case 622:
+                displayIcon = '/snow.png';
+                iconAlt = 'Heavy Snow';
+                break;
+            case 701: case 711: case 721: case 731: case 741: case 751: case 761: case 771:
+                displayIcon = '/mist.png';
+                iconAlt = 'Atmospheric';
+                break;
+            case 762:
+                displayIcon = '/mist.png';
+                iconAlt = 'Volcanic Ash';
+                break;
+            case 781:
+                displayIcon = '/mist.png';
+                iconAlt = 'Tornado';
+                break;
+            case 800:
+                displayIcon = '/sun.png';
+                iconAlt = 'Clear Sky';
+                break;
+            case 801:
+                displayIcon = '/few.png';
+                iconAlt = 'Few Clouds';
+                break;
+            case 802:
+                displayIcon = '/scattered.png';
+                iconAlt = 'Scattered Clouds';
+                break;
+            case 803:
+                displayIcon = '/broken.png';
+                iconAlt = 'Broken Clouds';
+                break;
+            case 804:
+                displayIcon = '/broken.png';
+                iconAlt = 'Overcast';
+                break;
+            default:
+                displayIcon = '/sun.png';
+                iconAlt = 'Undefined';
+          }
+          const embed = new MessageEmbed()
+            .setTitle("Current Weather in " + location.charAt(0).toUpperCase() + location.slice(1) + ", " + json.sys.country)
+            .setColor(0x008000)
+            .setFooter("Fetched from OpenWeatherMap by BackslashTBot", 'https://images-cdn.9gag.com/photo/aMj33oX_700b.jpg')
+            .addFields(
+              { name: 'Temperature', value: (json.main.temp - 273).toFixed(1) + " \u00b0" + "C", inline: true },
+              { name: 'Humidity', value: json.main.humidity + "%", inline: true },
+              { name: 'Weather', value: iconAlt, inline: true },
+            )
+            .addFields(
+              { name: 'Pressure', value: json.main.pressure + " hPa", inline: true },
+              { name: 'Wind', value: json.wind.deg + "\u00b0" + " At " + (json.wind.speed * 3.6).toFixed(1) + " km/h", inline: true },
+            )
+            .attachFiles(['./src' + displayIcon])
+            .setThumbnail('attachment:/' + displayIcon)
+            .setTimestamp();
+      	  message.channel.send(embed);
+          return json;
+        });     
 		} else if (command === 'f') {
+      location = commandArgs;
+      weather = getWeatherInfo(location);
+      //console.log(weather);
 			const embed = new MessageEmbed()
       			.setTitle("Bot")
       			.setColor(0xff0000)
       			.setDescription("Currently in testing :)")
-            .setFooter("bottom text", 'https://images-cdn.9gag.com/photo/aMj33oX_700b.jpg')
-            .setAuthor("big boy",'https://images-cdn.9gag.com/photo/aMj33oX_700b.jpg', 'https://www.google.ca')
+            .setFooter("Fetched from OpenWeatherMap by BackslashTBot", 'https://images-cdn.9gag.com/photo/aMj33oX_700b.jpg')
             .addFields(
-              { name: 'Temperature', value: (weathers.main.temp - 273).toFixed(1) + " C", inline: true },
-              { name: 'Humidity', value: weathers.main.humidity + "%", inline: true },
-              { name: 'Weather', value: weathers.weather[0].description, inline: true },
+              { name: 'Temperature', value: (weather.main.temp - 273).toFixed(1) + " \u00b0" + "C", inline: true },
+              { name: 'Humidity', value: weather.main.humidity + "%", inline: true },
+              { name: 'Weather', value: weather.weather[0].description.charAt(0).toUpperCase() + weather.weather[0].description.slice(1), inline: true },
             )
             .setImage('https://images-cdn.9gag.com/photo/aMj33oX_700b.jpg')
             .setThumbnail('https://images-cdn.9gag.com/photo/aMj33oX_700b.jpg')
